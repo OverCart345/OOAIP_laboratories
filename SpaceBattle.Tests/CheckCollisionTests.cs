c8974a94ab28cd623342b1b6943f9b23c46bbb4d
Ridiska <temich04om@mail.ru>
2023-12-27 00:38:35 +0600

    format fix

--- SpaceBattle.Tests/CheckCollisionTests.cs
+++ SpaceBattle.Tests/CheckCollisionTests.cs
@@ -1,6 +1,5 @@
 ﻿using Hwdtech;
 using Hwdtech.Ioc;
-using Moq;
 using ShipNamespace;
 
 namespace spacebattletests.StepDefinitions
@@ -17,15 +16,15 @@ namespace spacebattletests.StepDefinitions
 
         public void CollisionTestPositive()
         {
-            Mock<UniversalyObject> spaceship = new Mock<UniversalyObject>(new Dictionary<string, object>());
-            Mock<UniversalyObject> spaceship2 = new Mock<UniversalyObject>(new Dictionary<string, object>());
+            var spaceship = new Mock<UniversalyObject>(new Dictionary<string, object>());
+            var spaceship2 = new Mock<UniversalyObject>(new Dictionary<string, object>());
 
             spaceship.Object.properties.Add("Position", new Vector2d(1, 1));
             spaceship.Object.properties.Add("Velocity", new Vector2d(0, 0));
             spaceship2.Object.properties.Add("Position", new Vector2d(1, 1));
             spaceship2.Object.properties.Add("Velocity", new Vector2d(0, 0));
 
-            Mock<ICommand> CollisionCommand = new Mock<ICommand>();
+            var CollisionCommand = new Mock<ICommand>();
             CollisionCommand.Setup(c => c.Execute()).Verifiable();
             IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "CollisionCommand", (object[] args) => CollisionCommand.Object).Execute();
 
@@ -37,15 +36,15 @@ namespace spacebattletests.StepDefinitions
         [Fact]
         public void TryGetNewTreeThrowsException()
         {
-            Mock<UniversalyObject> spaceship = new Mock<UniversalyObject>(new Dictionary<string, object>());
-            Mock<UniversalyObject> spaceship2 = new Mock<UniversalyObject>(new Dictionary<string, object>());
+            var spaceship = new Mock<UniversalyObject>(new Dictionary<string, object>());
+            var spaceship2 = new Mock<UniversalyObject>(new Dictionary<string, object>());
 
             spaceship.Object.properties.Add("Position", new Vector2d(2, 4));
             spaceship.Object.properties.Add("Velocity", new Vector2d(1, 3));
             spaceship2.Object.properties.Add("Position", new Vector2d(1, 4));
             spaceship2.Object.properties.Add("Velocity", new Vector2d(1, 8));
 
-            Mock<ICommand> CollisionCommand = new Mock<ICommand>();
+            var CollisionCommand = new Mock<ICommand>();
             CollisionCommand.Setup(c => c.Execute()).Verifiable();
             IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "CollisionCommand", (object[] args) => CollisionCommand.Object).Execute();
 
--- SpaceBattle/CheckCollisionCommand.cs
+++ SpaceBattle/CheckCollisionCommand.cs
@@ -13,15 +13,17 @@ namespace ShipNamespace
         }
 
         public void Execute()
-        {    
-            Dictionary<string, object> objectOneProperties = objectOne.properties;
-            Dictionary<string, object> objectTwoProperties = objectTwo.properties;
+        {
+            var objectOneProperties = objectOne.properties;
+            var objectTwoProperties = objectTwo.properties;
 
-            Vector2d positionOne = (Vector2d)objectOneProperties["Position"] + (Vector2d)objectOneProperties["Velocity"];
-            Vector2d positionTwo = (Vector2d)objectTwoProperties["Position"] + (Vector2d)objectTwoProperties["Velocity"];
+            var positionOne = (Vector2d)objectOneProperties["Position"] + (Vector2d)objectOneProperties["Velocity"];
+            var positionTwo = (Vector2d)objectTwoProperties["Position"] + (Vector2d)objectTwoProperties["Velocity"];
 
-            if(positionOne.GetHashCode() != positionTwo.GetHashCode())
+            if (positionOne.GetHashCode() != positionTwo.GetHashCode())
+            {
                 return;
+            }
 
             IoC.Resolve<ICommand>("CollisionCommand", objectOne, objectTwo).Execute();
         }
