be3fd289504f763df105584c8be9bdb5e0fa3daf
Ridiska <temich04om@mail.ru>
2023-12-27 00:45:03 +0600

    one more fix...

--- SpaceBattle.Tests/CheckCollisionTests.cs
+++ SpaceBattle.Tests/CheckCollisionTests.cs
@@ -6,7 +6,7 @@ namespace spacebattletests.StepDefinitions
 {
     public class CheckCollisionTest
     {
-        public CheckCollisionTests()
+        public CheckCollisionTest()
         {
             new InitScopeBasedIoCImplementationCommand().Execute();
             IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
