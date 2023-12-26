c7e8ba88b1b4d0c932ff4cdbdf47286c4c18f36c
Erlan <erlanchuacbaev@gmail.com>
2023-12-26 17:06:02 +0600

    add tree

---
+++ SpaceBattle/Tree.cs
@@ -0,0 +1,37 @@
+﻿using Hwdtech;
+
+namespace SpaceBattle;
+
+public class Tree : ICommand
+{
+    private readonly string _path;
+    public Tree(string path)
+    {
+        _path = path;
+    }
+    public void Execute()
+    {
+        var parameters = new List<List<int>>();
+        var lines = File.ReadAllLines(_path);
+
+        foreach (var line in lines)
+        {
+            var values = line.Split().Select(int.Parse).ToList();
+            parameters.Add(values);
+        }
+
+        var builddecisiontree = IoC.Resolve<IDictionary<int, object>>("Game.CheckCollision");
+
+        parameters.ForEach(list =>
+        {
+            var decisiontree = builddecisiontree;
+            list.ForEach(coordinate =>
+            {
+                decisiontree.TryAdd(coordinate, new Dictionary<int, object>());
+                decisiontree = (Dictionary<int, object>)decisiontree[coordinate];
+            }
+            );
+        }
+        );
+    }
+}
