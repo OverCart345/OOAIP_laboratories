df9ddc4bcb14483e9d6411de4a5f1b6ef11c0588
Ridiska <temich04om@mail.ru>
2024-03-25 21:23:04 +0600

    delete excess file

--- SpaceBattle/Order.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ShipNamespace
-{
-    public interface Order
-    {
-        UniversalyObject Target { get; }
-        string Command { get; }
-        Vector2d Velocity { get; }
-    }
-}
