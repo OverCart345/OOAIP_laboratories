46ca8c48fafb53733ee5e2875337252831970e59
Ridiska <temich04om@mail.ru>
2024-04-30 22:15:02 +0600

    lalala

---
+++ SpaceBattle/EndPoint.cs
@@ -0,0 +1,45 @@
+﻿using Hwdtech;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace ShipNamespace
+{
+    public class Endpoint
+    {
+        private static WebApplication? webApplication;
+        private static Action? setCurrentScope;
+
+        public static void InitScope(Action newScope)
+        {
+            setCurrentScope = newScope;
+        }
+        public static void InitAndStart()
+        {
+            var webApplicationBuilder = WebApplication.CreateBuilder();
+            webApplication = webApplicationBuilder.Build();
+            webApplication.UseHttpsRedirection();
+            webApplication.MapPost("/message", (Message message) =>
+            {
+                if (setCurrentScope != null)
+                    setCurrentScope();
+
+                try
+                {
+
+                    if (Guid.TryParse(message.Fields["game id"].ToString(), out var gameId))
+                    {
+                        var fakeCommand = IoC.Resolve<IComand>("executeFakeCommand");
+                        IoC.Resolve<IComand>("Send Command", gameId, fakeCommand).Execute();
+                    }
+
+                    return Results.Ok();
+                }
+                catch
+                {
+                    return Results.BadRequest();
+                }
+            });
+            webApplication.RunAsync("http://localhost:5050");
+        }
+    } 
+}
---
+++ SpaceBattle/Message.cs
@@ -0,0 +1,12 @@
+﻿namespace ShipNamespace
+{
+    public class Message
+    {
+        public IDictionary<string, object> Fields { get; set; } = new Dictionary<string, object>();
+
+        public Message(IDictionary<string, object> fields)
+        {
+            Fields = fields;
+        }
+    }
+}
