b4b48efac2c32e1cc84e19b5f7a2cd3bc51889fa
Ridiska <temich04om@mail.ru>
2024-05-27 01:06:26 +0600

    fiasd

---
+++ SpaceBattle/Handler.cs
@@ -0,0 +1,24 @@
+﻿using Hwdtech;
+
+namespace ShipNamespace
+{
+    public interface IHandler
+    {
+        void Handle();
+    }
+
+    public class DefaultHandler : IHandler
+    {
+        private readonly Exception _exception;
+
+        public DefaultHandler(Exception e)
+        {
+            _exception = e;
+        }
+
+        public void Handle()
+        {
+            throw _exception;
+        }
+    }
+}
